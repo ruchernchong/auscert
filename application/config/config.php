1d150f17f04b09336fa2d7856664e8ee25451d0b
Ru Chern <iruchern@gmail.com>
2015-08-05 16:22:32 +1000

    Redirect base URL from UQCloud to localhost

--- application/config/config.php
+++ application/config/config.php
@@ -16,7 +16,8 @@
 | environments.
 |
 */
-$config['base_url'] = 'http://deco3800-redones.uqcloud.net/';
+// $config['base_url'] = 'http://deco3800-redones.uqcloud.net/';
+$config['base_url'] = 'localhost/redones';
 
 /*
 |--------------------------------------------------------------------------
