c56a90af5155f5cbbc6bea002a26c163b10c3c7e
Ru Chern <iruchern@gmail.com>
2015-08-07 01:46:52 +1000

    Moved js files

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,40 +1,142 @@
-var selected1;
+var selected;
 
 $(document).ready(function(){ //called once the page has loaded
-	document.getElementById('content').style.display="none";
-	document.getElementById('text1').style.display="none";
+	document.getElementById('mail1').style.color='#0066FF';
+	document.getElementById('text1').style.display="block";
 	document.getElementById('text2').style.display="none";
-	document.getElementById('delete').style.display="none";
+	document.getElementById('text3').style.display="none";
+	document.getElementById('txtreply').style.display="none";
+	document.getElementById('options2').style.display="none";
+	selected = "mail1";
+	
+	function resetoptions(){
+		document.getElementById('txtreply').style.display="none";
+		document.getElementById('options2').style.display="none";
+		document.getElementById('options').style.display="block";
+	}
+	
+	function showJohn() {
+		document.getElementById('mail2').style.color='#0066FF';
+		document.getElementById('text2').style.display="block";
+	}
 	
 	$("#mail1").click(function(){ //first email
-		$("#title").text('Hey, this is your friend.'); //embed a youtube video
-		document.getElementById('content').style.display="block";
+		resetoptions();
 		document.getElementById('mail1').style.color='#0066FF';
 		document.getElementById('mail2').style.color='#000';
+		document.getElementById('mail3').style.color='#000';
 		document.getElementById('text1').style.display="block";
 		document.getElementById('text2').style.display="none";
-		document.getElementById('delete').style.display="block";
-		selected1 = "mail1";
+		document.getElementById('text3').style.display="none";
+		selected = "mail1";
 	});
 	
 	$("#mail2").click(function(){ //second email
-		$("#title").text('Quick, the Universe is in need of saving!'); //embed a youtube video
-		document.getElementById('content').style.display="block";
+		resetoptions();
 		document.getElementById('mail1').style.color='#000';
 		document.getElementById('mail2').style.color='#0066FF';
+		document.getElementById('mail3').style.color='#000';
 		document.getElementById('text1').style.display="none";
 		document.getElementById('text2').style.display="block";
-		document.getElementById('delete').style.display="block";
-		selected1 = "mail2";
+		document.getElementById('text3').style.display="none";
+		selected = "mail2";
 	});
 	
-	$("#delete").click(function(){ //second email
-		if (selected1 == "mail1") {
-			window.alert("Well Done!");
-		}else if (selected1 == "mail2") {
-			window.alert("Wrong.");
-		}else{
-			window.alert("Error.");
+	$("#mail3").click(function(){ //third email
+		resetoptions();
+		document.getElementById('mail1').style.color='#000';
+		document.getElementById('mail2').style.color='#000';
+		document.getElementById('mail3').style.color='#0066FF';
+		document.getElementById('text1').style.display="none";
+		document.getElementById('text2').style.display="none";
+		document.getElementById('text3').style.display="block";
+		selected = "mail3";
+	});
+	
+	$("#delete").click(function(){ //delete
+		if (selected == "mail1") {
+			$( ".ui-widget-header" ).css( "background", "#33CC33" );
+			$( "#dialog-correct1" ).dialog( "open" );
+			document.getElementById('mail1').style.display='none';
+			document.getElementById('text1').style.display="none";
+			selected = "mail2";
+			showJohn();
+		}else if (selected == "mail3") {
+			$( ".ui-widget-header" ).css( "background", "#33CC33" );
+			$( "#dialog-correct2" ).dialog( "open" );
+			document.getElementById('mail3').style.display='none';
+			document.getElementById('text3').style.display="none";
+			selected = "mail2";
+			showJohn();
+		}
+	});
+	
+	$("#reply").click(function(){ //reply
+		if (selected == "mail1") {
+			document.getElementById('txtreply').style.display="block";
+			document.getElementById('text1').style.display="none";
+			document.getElementById('options2').style.display="block";
+			document.getElementById('options').style.display="none";
+		}
+	});
+	
+	$("#cancel").click(function(){ //cancel
+		resetoptions();
+		document.getElementById('mail1').style.color='#0066FF';
+		document.getElementById('text1').style.display="block";
+	});
+	
+	$("#send").click(function(){ //send
+		$( ".ui-widget-header" ).css( "background", "#FF3333" );
+		$( "#dialog-wrong1" ).dialog( "open" );
+	});
+	
+	$("#link").click(function(){ //send
+		$( ".ui-widget-header" ).css( "background", "#FF3333" );
+		$( "#dialog-wrong2" ).dialog( "open" );
+	});
+});
+
+$(function() {
+	$("#dialog-correct1").dialog({
+		autoOpen: false,
+		dialogClass: "no-close",
+		modal: true,
+		buttons: {
+			Ok: function() { $(this).dialog("close"); }
+		}
+	});
+});
+
+$(function() {
+	$("#dialog-correct2").dialog({
+		autoOpen: false,
+		dialogClass: "no-close",
+		modal: true,
+		buttons: {
+			Ok: function() { $(this).dialog("close"); }
+		}
+	});
+});
+
+$(function() {
+	$("#dialog-wrong1").dialog({
+		autoOpen: false,
+		dialogClass: "no-close",
+		modal: true,
+		buttons: {
+			Ok: function() { $(this).dialog("close"); }
+		}
+	});
+});
+
+$(function() {
+	$("#dialog-wrong2").dialog({
+		autoOpen: false,
+		dialogClass: "no-close",
+		modal: true,
+		buttons: {
+			Ok: function() { $(this).dialog("close"); }
 		}
 	});
 });
