d855b5843819a9f0bd4909cddaf3fa229819cc57
Ru Chern <iruchern@gmail.com>
2015-08-07 00:47:33 +1000

    Added footer to view controller

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -3,78 +3,79 @@
 
 class Home extends CI_Controller {
 	function __construct(){
-        parent::__construct();
-	$this->load->model('course_model');
+		parent::__construct();
+		$this->load->model('course_model');
 
-    }
-    public function index(){
-  
-         if($this->session->userdata('logged_in'))
-		   {
-			 $session_data = $this->session->userdata('logged_in');
-			 $data['username'] = $session_data['username'];
-			 $data['menu'] = "home";
-			 $this->load->view('header',$data);
-			 $this->load->view('test1');
-		   }
-		   else
-		   {
+	}
+	public function index(){
+		
+		if($this->session->userdata('logged_in'))
+		{
+			$session_data = $this->session->userdata('logged_in');
+			$data['username'] = $session_data['username'];
+			$data['menu'] = "home";
+			$this->load->view('header',$data);
+			$this->load->view('dashboard');
+			$this->load->view("footer");
+		}
+		else
+		{
 			 //If no session, redirect to login page
-			 redirect('welcome', 'refresh');
-		   }
-    }
-   
+			redirect('welcome', 'refresh');
+		}
+	}
+	
 	function logout()
-			 {
-				 
-			   $this->session->unset_userdata('logged_in');
-			   redirect('home','refresh');
-			   session_destroy();
-			 }
- 
+	{
+		
+		$this->session->unset_userdata('logged_in');
+		redirect('home','refresh');
+		session_destroy();
+	}
+	
 	
 	function mygrade()
-			 {
-				 if($this->session->userdata('logged_in'))
-		   {
-			 $session_data = $this->session->userdata('logged_in');
-			 $data['username'] = $session_data['username'];
-			 $data['menu'] = "mygrade";
-			 $this->load->view('header',$data);
-			 $this->load->view('mygrade_view');
-		   }
-		   else
-		   {
+	{
+		if($this->session->userdata('logged_in'))
+		{
+			$session_data = $this->session->userdata('logged_in');
+			$data['username'] = $session_data['username'];
+			$data['menu'] = "mygrade";
+			$this->load->view('header',$data);
+			$this->load->view('mygrade_view');
+		}
+		else
+		{
 			 //If no session, redirect to login page
-			 redirect('welcome', 'refresh');
-		   } 
-			  
-			 }
-			 
+			redirect('welcome', 'refresh');
+		} 
+		
+	}
+	
 
-			 
-	 function adminpage()
-		 {
-			 if($this->session->userdata('logged_in'))
-	   {
-		 $session_data = $this->session->userdata('logged_in');
-		 $data['username'] = $session_data['username'];
-		 $data['menu'] = "adminpage";
-		 $query = $this->course_model->GetCourse();
-                         if ($query){
-                              $data['lessons'] = $query;
-                         }
+	
+	function adminpage()
+	{
+		if($this->session->userdata('logged_in'))
+		{
+			$session_data = $this->session->userdata('logged_in');
+			$data['username'] = $session_data['username'];
+			$data['menu'] = "adminpage";
+			$query = $this->course_model->GetCourse();
+			if ($query){
+				$data['lessons'] = $query;
+			}
 
-		 $this->load->view('header',$data);
-		 $this->load->view('adminpage_view',$data);
-	   }
-	   else
-	   {
+			$this->load->view('header',$data);
+			$this->load->view('adminpage_view',$data);
+		}
+		else
+		{
 		 //If no session, redirect to login page
-		 redirect('welcome', 'refresh');
-	   } 
-		  
-		 }
+			redirect('welcome', 'refresh');
+		} 
+		
+	}
 
 }
 
