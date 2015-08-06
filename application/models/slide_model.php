bc7da6b4efec8445ccb8871c8273b1487f1ceb9f
Ru Chern <iruchern@gmail.com>
2015-08-07 01:48:04 +1000

    Code cleanup

--- application/models/slide_model.php
+++ application/models/slide_model.php
@@ -2,30 +2,32 @@
 
 Class slide_model extends CI_Model{
 	function __construct(){
-        parent::__construct();
-    }
-    
-    public function GetSlideById(){
-       $this->db->where('lessonID',$this->input->get('lid'));
-       $this->db->where('sid',$this->input->get('sid'));
-        $query = $this->db->get('slide');
-        //var_dump($query->num_rows);
-        if ($query->num_rows == 1)
+		parent::__construct();
+	}
+
+	public function GetSlideById(){
+		$this->db->where('lessonID',$this->input->get('lid'));
+		$this->db->where('sid',$this->input->get('sid'));
+		$query = $this->db->get('slide');
+		//var_dump($query->num_rows);
+		
+		if ($query->num_rows == 1)
 		{
 			return $query->result();
-        }return false;
-    }
-    
-    public function GetSlide(){
-       $this->db->where('lessonID',$this->input->get('lid'));
-       
-       //$this->db->where('sid',$this->input->get('sid'));
-        $query = $this->db->get('slide');
-        //var_dump($query->num_rows);
-        if ($query->num_rows >= 1)
+		}return false;
+	}
+
+	public function GetSlide(){
+		$this->db->where('lessonID',$this->input->get('lid'));
+
+		//$this->db->where('sid',$this->input->get('sid'));
+		$query = $this->db->get('slide');
+		//var_dump($query->num_rows);
+
+		if ($query->num_rows >= 1)
 		{
 			return $query->result();
-        }return false;
-    }
+		}return false;
+	}
 }
 ?>
