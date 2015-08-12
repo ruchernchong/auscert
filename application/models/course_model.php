d250a32d9cbd453a377121120dbcd8610ee36071
Ru Chern <iruchern@gmail.com>
2015-08-12 14:47:46 +1000

    Fixed indentation

--- application/models/course_model.php
+++ application/models/course_model.php
@@ -1,39 +1,40 @@
 <?php
 
-Class course_model extends CI_Model{
-	function __construct(){
-        parent::__construct();
-    }
-    
-    public function validate(){
-        $this->db->where('userID',$this->input->post('username'));
-        $this->db->where('password',$this->input->post('password'));
-        
-        $query = $this->db->get('user');
+Class course_model extends CI_Model {
+	function __construct() {
+		parent::__construct();
+	}
+
+	public function validate() {
+		$this->db->where('username',$this->input->post('username'));
+		$this->db->where('password',$this->input->post('password'));
+
+		$query = $this->db->get('user');
         //var_dump($query->num_rows);
-        if ($query->num_rows == 1)
+		if ($query->num_rows == 1)
 		{
 			return $query->result();
-        }return false;
-    }
+		}
+		return false;
+	}
 
-    public function GetCourse(){
-        $query = $this->db->get('lessons');
+	public function GetCourse() {
+		$query = $this->db->get('lessons');
         //var_dump($query->num_rows);
-        if ($query->num_rows >= 1)
-        {
-           return $query->result();
-        }
-        return false;
-    }
-    
-    public function GetCourseById(){
-           $this->db->where('lessonID',$this->input->get('lid'));
-      $query = $this->db->get('lessons');
-      if ($query->num_rows == 1){
-        return $query->result();
-      }
-      return false;
-    }
+		if ($query->num_rows >= 1)
+		{
+			return $query->result();
+		}
+		return false;
+	}
+
+	public function GetCourseById() {
+		$this->db->where('lessonID',$this->input->get('lid'));
+		$query = $this->db->get('lessons');
+		if ($query->num_rows == 1){
+			return $query->result();
+		}
+		return false;
+	}
 }
 ?>
--- application/views/view_learning.php
+++ application/views/view_learning.php
@@ -1,7 +1,5 @@
 <div id="page-wrapper">
-
 	<div class="container-fluid">
-
 		<!-- Page Heading -->
 		<div class="row">
 			<div class="col-lg-12">
