46a1a1a625fc1f03317aca0a61132032d4a4ead2
Ru Chern <iruchern@gmail.com>
2015-08-12 15:00:49 +1000

    Fixed changes made in database but not here.

--- application/models/slide_model.php
+++ application/models/slide_model.php
@@ -1,33 +1,33 @@
 <?php
 
-Class slide_model extends CI_Model{
+Class slide_model extends CI_Model {
 	function __construct(){
 		parent::__construct();
 	}
 
-	public function GetSlideById(){
+	public function GetSlideById() {
 		$this->db->where('lessonID',$this->input->get('lid'));
-		$this->db->where('sid',$this->input->get('sid'));
+		$this->db->where('slideID',$this->input->get('sid'));
 		$query = $this->db->get('slide');
 		//var_dump($query->num_rows);
 		
-		if ($query->num_rows == 1)
-		{
+		if ($query->num_rows == 1) {
 			return $query->result();
-		}return false;
+		}
+		return false;
 	}
 
-	public function GetSlide(){
+	public function GetSlide() {
 		$this->db->where('lessonID',$this->input->get('lid'));
 
 		//$this->db->where('sid',$this->input->get('sid'));
 		$query = $this->db->get('slide');
 		//var_dump($query->num_rows);
 
-		if ($query->num_rows >= 1)
-		{
+		if ($query->num_rows >= 1) {
 			return $query->result();
-		}return false;
+		}
+		return false;
 	}
 }
 ?>
