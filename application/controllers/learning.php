d5becb75139d2fb33e9688426854580d31203d74
Ru Chern <iruchern@gmail.com>
2015-08-12 14:47:19 +1000

    Fixed indentation

--- application/controllers/learning.php
+++ application/controllers/learning.php
@@ -3,38 +3,34 @@
 
 class Learning extends CI_Controller {
 	function __construct(){
-        parent::__construct();
-	$this->load->model('course_model');
-        	$this->load->model('slide_model');
-    }
-    public function index(){
-  
-         if($this->session->userdata('logged_in'))
-		   {
-			 $session_data = $this->session->userdata('logged_in');
-			 $data['username'] = $session_data['username'];
-			 $data['menu'] = "course";
-                         $query = $this->course_model->GetCourseById();
-                         if ($query){
-                              $data['lessons'] = $query;
-                         }
-                         $slides = $this->slide_model->GetSlide();
-                         if ($slides){
-                           $data['slides'] = $slides;
-                         }
+		parent::__construct();
+		$this->load->model('course_model');
+		$this->load->model('slide_model');
+	}
+	public function index(){
+
+		if($this->session->userdata('logged_in'))
+		{
+			$session_data = $this->session->userdata('logged_in');
+			$data['username'] = $session_data['username'];
+			$data['menu'] = "course";
+			$query = $this->course_model->GetCourseById();
+			if ($query){
+				$data['lessons'] = $query;
+			}
+			$slides = $this->slide_model->GetSlide();
+			if ($slides){
+				$data['slides'] = $slides;
+			}
                          //var_dump($slides);
-			 $this->load->view('header',$data);
-			 $this->load->view('view_learning',$data);
-		   }
-		   else
-		   {
+			$this->load->view('header',$data);
+			$this->load->view('view_learning',$data);
+		}
+		else
+		{
 			 //If no session, redirect to login page
-			 redirect('welcome', 'refresh');
-		   }
-    }
-    
-
+			redirect('welcome', 'refresh');
+		}
+	}
 }
-
-
 ?>
