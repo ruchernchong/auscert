d571f37d4f882349942c3a9e6168a3360873149d
Ru Chern <iruchern@gmail.com>
2015-08-17 16:48:20 +1000

    Added new columns needed to run the admin page

--- sqlfiles/auscertdb.sql
+++ sqlfiles/auscertdb.sql
@@ -3,49 +3,142 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 12, 2015 at 06:24 AM
+-- Generation Time: Aug 17, 2015 at 08:47 AM
 -- Server version: 5.6.21
 -- PHP Version: 5.6.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-CREATE TABLE IF NOT EXISTS `user` (
-  `userID` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(65) NOT NULL,
-  `password` varchar(65) NOT NULL,
-  `email` varchar(65) NOT NULL,
-  `userType` varchar(24) NOT NULL,
-  PRIMARY KEY (`userID`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 
-INSERT INTO `user` (`userID`, `username`, `password`, `email`, `userType`) VALUES
-(1, 'admin', 'admin', 'admin@gmail.com', 'admin'),
-(2, 'leon', 'admin', 'leonxenarax@gmail.com', 'creator'),
-(3, 'ruchern', 'admin', 'iruchern@gmail.com', 'admin'),
-(4, 'helen', 'admin', 'helen@gmail.com', 'user'),
-(5, 'cameron', 'admin', 'cameron@gmail.com', 'user'),
-(6, 'ravi', 'admin', 'ravi@gmail.com', 'user'),
-(7, 'mal', 'admin', 'mal.joseland@live.com', 'user');
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
+--
+-- Database: `auscertdb`
+--
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `courses`
+--
 
 CREATE TABLE IF NOT EXISTS `courses` (
-  `courseID` int(11) NOT NULL AUTO_INCREMENT,
+`courseID` int(11) NOT NULL,
   `courseName` varchar(255) NOT NULL,
   `category` varchar(255) DEFAULT NULL,
   `creator` varchar(255) DEFAULT NULL,
-  `active` boolean NOT NULL,
+  `active` tinyint(1) NOT NULL,
   `description` longtext,
-  PRIMARY KEY (`courseID`)
+  `last_edited` varchar(255) NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
-INSERT INTO `courses` (`courseID`, `courseName`, `category`, `creator`, `active`, `description`) VALUES
-(1, 'Phising Email', 'Online Safety', 'Tartiner Studios', 0, 'Introduciton to the phishing emails. What they are and what you can do to avoid being a victim'),
-(2, 'Choosing A Safe Password', 'Account Security', 'Redones', 1, 'This course will guide you through how a password works as well as steps to take to ensure a strong and secure password'),
-(3, 'Tartiner Studios Training', 'Introduction to the team', 'Tartiner Studios', 1, 'Self made course designed by team tartiner on the importance of spreading nutella the RIGHT way on bread');
+--
+-- Dumping data for table `courses`
+--
 
+INSERT INTO `courses` (`courseID`, `courseName`, `category`, `creator`, `active`, `description`, `last_edited`) VALUES
+(1, 'Phising Email', 'Online Safety', 'Tartiner Studios', 0, 'Introduciton to the phishing emails. What they are and what you can do to avoid being a victim', '8/03/2015'),
+(2, 'Choosing A Safe Password', 'Account Security', 'Redones', 1, 'This course will guide you through how a password works as well as steps to take to ensure a strong and secure password', '19/7/2015'),
+(3, 'Tartiner Studios Training', 'Introduction to the team', 'Tartiner Studios', 1, 'Self made course designed by team tartiner on the importance of spreading nutella the RIGHT way on bread', '17/8/2015');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `questions`
+--
+
+CREATE TABLE IF NOT EXISTS `questions` (
+`questionID` int(11) NOT NULL,
+  `quizID` int(11) NOT NULL,
+  `questionText` text NOT NULL,
+  `options` text NOT NULL,
+  `answer` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `quiz`
+--
+
+CREATE TABLE IF NOT EXISTS `quiz` (
+`quizID` int(11) NOT NULL,
+  `courseID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `slide`
+--
+
+CREATE TABLE IF NOT EXISTS `slide` (
+`slideID` int(11) NOT NULL,
+  `courseID` int(11) NOT NULL,
+  `slideOrder` int(3) NOT NULL,
+  `slideContent` text,
+  `slideTitle` varchar(50) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `slide`
+--
+
+INSERT INTO `slide` (`slideID`, `courseID`, `slideOrder`, `slideContent`, `slideTitle`) VALUES
+(1, 1, 1, 'Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus\r\nterry richardson ad squid. 3 wolf moon officia aute, non cupidatat\r\n                  skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod.\r\n                  Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid\r\n                  single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh\r\n                  helvetica, craft beer labore wes anderson cred nesciunt sapiente ea\r\n                  proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft\r\n                  beer farm-to-table, raw denim aesthetic synth nesciunt you probably\r\n                  haven''t heard of them accusamus labore sustainable VHS.', 'Introduction to Phishing Email'),
+(2, 1, 2, 'Team Tartiner Testing Content of Further Study', 'Special Phishing Emails'),
+(3, 2, 1, 'course 2 Page 1', 'Course 2 Page 1'),
+(4, 2, 2, 'Course 2 Page 2', 'Course 2 Page 2');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+`userID` int(11) NOT NULL,
+  `username` varchar(65) NOT NULL,
+  `password` varchar(65) NOT NULL,
+  `email` varchar(65) NOT NULL,
+  `contact` varchar(255) NOT NULL,
+  `userType` varchar(24) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`userID`, `username`, `password`, `email`, `contact`, `userType`) VALUES
+(1, 'admin', 'admin', 'admin@gmail.com', '000', 'admin'),
+(2, 'leon', 'admin', 'leonxenarax@gmail.com', '000', 'creator'),
+(3, 'ruchern', 'admin', 'iruchern@gmail.com', '0451 519 513', 'admin'),
+(4, 'helen', 'admin', 'helen@gmail.com', '000', 'user'),
+(5, 'cameron', 'admin', 'cameron@gmail.com', '000', 'user'),
+(6, 'ravi', 'admin', 'ravi@gmail.com', '000', 'user'),
+(7, 'mal', 'admin', 'mal.joseland@live.com', '000', 'user');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usergroups`
+--
+
+CREATE TABLE IF NOT EXISTS `usergroups` (
+`usergroupID` int(11) NOT NULL,
+  `organisation` varchar(255) NOT NULL,
+  `members` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_courses`
+--
 
 CREATE TABLE IF NOT EXISTS `user_courses` (
   `userID` int(11) NOT NULL,
@@ -53,60 +146,127 @@ CREATE TABLE IF NOT EXISTS `user_courses` (
   `completion` decimal(5,2) NOT NULL,
   `description` text NOT NULL,
   `grading` varchar(255) NOT NULL,
-  `mandatory` boolean NOT NULL,
-  PRIMARY KEY (`userID`, `courseID`),
-  FOREIGN KEY (`userID`) REFERENCES user(`userID`),
-  FOREIGN KEY (`courseID`) REFERENCES courses(`courseID`)
+  `mandatory` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO `user_courses` (`userID`, `courseID`,`completion`, `description`, `grading`, `mandatory`) VALUES
+--
+-- Dumping data for table `user_courses`
+--
+
+INSERT INTO `user_courses` (`userID`, `courseID`, `completion`, `description`, `grading`, `mandatory`) VALUES
 (2, 1, '20.00', '', '80', 1),
-(2, 3, '100.00', '', '90', 0);
+(2, 3, '100.00', '', '90', 0),
+(3, 1, '100.00', 'Testing 1', '100', 1);
 
+--
+-- Indexes for dumped tables
+--
 
-CREATE TABLE IF NOT EXISTS `usergroups` (
-`usergroupID` int(11) NOT NULL AUTO_INCREMENT,
-`organisation` varchar(255) NOT NULL,
-`members` varchar(255),
-PRIMARY KEY (`usergroupID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+--
+-- Indexes for table `courses`
+--
+ALTER TABLE `courses`
+ ADD PRIMARY KEY (`courseID`);
 
+--
+-- Indexes for table `questions`
+--
+ALTER TABLE `questions`
+ ADD PRIMARY KEY (`questionID`), ADD KEY `quizID` (`quizID`);
 
+--
+-- Indexes for table `quiz`
+--
+ALTER TABLE `quiz`
+ ADD PRIMARY KEY (`quizID`), ADD KEY `courseID` (`courseID`);
 
-CREATE TABLE IF NOT EXISTS `quiz` (
-  `quizID` int(11) NOT NULL AUTO_INCREMENT,
-  `courseID` int(11) NOT NULL,
-  PRIMARY KEY (`quizID`),
-  FOREIGN KEY (`courseID`) REFERENCES courses(`courseID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+--
+-- Indexes for table `slide`
+--
+ALTER TABLE `slide`
+ ADD PRIMARY KEY (`slideID`), ADD KEY `courseID` (`courseID`);
 
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+ ADD PRIMARY KEY (`userID`);
 
+--
+-- Indexes for table `usergroups`
+--
+ALTER TABLE `usergroups`
+ ADD PRIMARY KEY (`usergroupID`);
 
-CREATE TABLE IF NOT EXISTS `questions` (
-  `questionID` int(11) NOT NULL AUTO_INCREMENT,
-  `quizID` int(11) NOT NULL,
-  `questionText` text NOT NULL,
-  `options` text NOT NULL,
-  `answer` int(11) NOT NULL,
-  PRIMARY KEY (`questionID`),
-  FOREIGN KEY (`quizID`) REFERENCES quiz(`quizID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+--
+-- Indexes for table `user_courses`
+--
+ALTER TABLE `user_courses`
+ ADD PRIMARY KEY (`userID`,`courseID`), ADD KEY `courseID` (`courseID`);
 
+--
+-- AUTO_INCREMENT for dumped tables
+--
 
+--
+-- AUTO_INCREMENT for table `courses`
+--
+ALTER TABLE `courses`
+MODIFY `courseID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `questions`
+--
+ALTER TABLE `questions`
+MODIFY `questionID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `quiz`
+--
+ALTER TABLE `quiz`
+MODIFY `quizID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `slide`
+--
+ALTER TABLE `slide`
+MODIFY `slideID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
+--
+-- AUTO_INCREMENT for table `usergroups`
+--
+ALTER TABLE `usergroups`
+MODIFY `usergroupID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Constraints for dumped tables
+--
 
-CREATE TABLE IF NOT EXISTS `slide` (
-  `slideID` int(11) NOT NULL AUTO_INCREMENT,
-  `courseID` int(11) NOT NULL,
-  `slideOrder` int(3) NOT NULL,
-  `slideContent` text,
-  `slideTitle` varchar(50) NOT NULL,
-  PRIMARY KEY (`slideID`),
-  FOREIGN KEY (`courseID`) REFERENCES courses(`courseID`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+--
+-- Constraints for table `questions`
+--
+ALTER TABLE `questions`
+ADD CONSTRAINT `questions_ibfk_1` FOREIGN KEY (`quizID`) REFERENCES `quiz` (`quizID`);
 
-INSERT INTO `slide` (`courseID`, `slideID`, `slideOrder`, `slideContent`, `slideTitle`) VALUES
-(1, 1, 1, 'Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus\r\nterry richardson ad squid. 3 wolf moon officia aute, non cupidatat\r\n                  skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod.\r\n                  Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid\r\n                  single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh\r\n                  helvetica, craft beer labore wes anderson cred nesciunt sapiente ea\r\n                  proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft\r\n                  beer farm-to-table, raw denim aesthetic synth nesciunt you probably\r\n                  haven''t heard of them accusamus labore sustainable VHS.', 'Introduction to Phishing Email'),
-(1, 2, 2, 'Team Tartiner Testing Content of Further Study', 'Special Phishing Emails'),
-(2, 3, 1, 'course 2 Page 1', 'Course 2 Page 1'),
-(2, 4, 2, 'Course 2 Page 2', 'Course 2 Page 2');
+--
+-- Constraints for table `quiz`
+--
+ALTER TABLE `quiz`
+ADD CONSTRAINT `quiz_ibfk_1` FOREIGN KEY (`courseID`) REFERENCES `courses` (`courseID`);
 
+--
+-- Constraints for table `slide`
+--
+ALTER TABLE `slide`
+ADD CONSTRAINT `slide_ibfk_1` FOREIGN KEY (`courseID`) REFERENCES `courses` (`courseID`);
+
+--
+-- Constraints for table `user_courses`
+--
+ALTER TABLE `user_courses`
+ADD CONSTRAINT `user_courses_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `user` (`userID`),
+ADD CONSTRAINT `user_courses_ibfk_2` FOREIGN KEY (`courseID`) REFERENCES `courses` (`courseID`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
