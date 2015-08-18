ceee011aa0c3119de66d144049793bd2b46c61b6
Ru Chern <iruchern@gmail.com>
2015-08-18 20:10:27 +1000

    Update

--- application/views/header.php
+++ application/views/header.php
@@ -1,61 +1,58 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="description" content="">
-	<meta name="author" content="">
+	<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="">
+		<meta name="author" content="">
 
-	<title>AusCert | Dashboard</title>
+		<title>AusCert | Dashboard</title>
 
-	<!-- Bootstrap Core CSS -->
-	<link href="<?php echo base_url('assets/css/bootstrap.min.css'); ?>" rel="stylesheet">
-	<link href="<?php echo base_url('assets/css/sb-admin.css'); ?>" rel="stylesheet">
-	<link href="<?php echo base_url('assets/css/sb-admin-rtl.css'); ?>" rel="stylesheet">
-	<link href="<?php echo base_url('assets/css/quiz.css'); ?>" rel="stylesheet">
-	<link href="<?php echo base_url('assets/css/jquery-ui.min.css'); ?>" rel="stylesheet">
-	<link href="<?php echo base_url('assets/css/email.css'); ?>" rel="stylesheet">
-	<link href="<?php echo base_url('assets/css/font-awesome.min.css'); ?>" rel="stylesheet" type="text/css">
+		<!-- Bootstrap Core CSS -->
+		<link href="<?php echo base_url('assets/css/bootstrap.min.css'); ?>" rel="stylesheet">
+		<link href="<?php echo base_url('assets/css/sb-admin.css'); ?>" rel="stylesheet">
+		<link href="<?php echo base_url('assets/css/sb-admin-rtl.css'); ?>" rel="stylesheet">
+		<link href="<?php echo base_url('assets/css/quiz.css'); ?>" rel="stylesheet">
+		<link href="<?php echo base_url('assets/css/jquery-ui.min.css'); ?>" rel="stylesheet">
+		<link href="<?php echo base_url('assets/css/email.css'); ?>" rel="stylesheet">
+		<link href="<?php echo base_url('assets/css/font-awesome.min.css'); ?>" rel="stylesheet" type="text/css">
 
-	<script src="<?php echo base_url('assets/js/jquery-1.11.3.min.js') ?>"></script>
-	<script src="<?php echo base_url('assets/ckeditor/ckeditor.js'); ?>"></script>
-	<script src="<?php echo base_url('assets/js/quiz.js'); ?>"></script>
-	<script src="<?php echo base_url('assets/js/jquery-ui.min.js'); ?>"></script>
-	<script src="<?php echo base_url('assets/js/dash.js'); ?>"></script>
-</head>
+		<script src="<?php echo base_url('assets/js/jquery-1.11.3.min.js') ?>"></script>
+		<script src="<?php echo base_url('assets/ckeditor/ckeditor.js'); ?>"></script>
+		<script src="<?php echo base_url('assets/js/quiz.js'); ?>"></script>
+		<script src="<?php echo base_url('assets/js/jquery-ui.min.js'); ?>"></script>
+		<script src="<?php echo base_url('assets/js/dash.js'); ?>"></script>
+	</head>
 
-<body>
-	<?php date_default_timezone_set("Australia/Brisbane"); ?>
-	<div id="wrapper">
-		<!-- Navigation -->
-		<!-- <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation"> -->
-		<!-- Brand and toggle get grouped for better mobile display -->
-			<!-- <div class="navbar-header">
-				<img src="<?php echo base_url('assets/img/uq_logo.png'); ?>" class="uq-logo">
-			</div>
-
-			<ul class="nav navbar-right top-nav">
-				<li class="dropdown">
-					<a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i>&emsp;<?php echo $username;?> <b class="caret"></b></a>
-					<ul class="dropdown-menu">
-						<li>
-							<a href="#"><i class="fa fa-fw fa-user"></i>&emsp;Profile</a>
-						</li>
-						<li>
-							<a href="#"><i class="fa fa-fw fa-envelope"></i>&emsp;Inbox</a>
-						</li>
-						<li>
-							<a href="#"><i class="fa fa-fw fa-gear"></i>&emsp;Settings</a>
-						</li>
-						<li class="divider"></li>
-						<li>
-							<a href="<?php echo site_url('home/logout') ?>"><i class="fa fa-fw fa-power-off"></i>&emsp;Log Out</a>
-						</li>
-					</ul>
-				</li>
-			</ul> --> 
-			<!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
+	<body>
+		<?php date_default_timezone_set("Australia/Brisbane"); ?>
+		<div id="wrapper">
+			<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+				<div class="navbar-header">
+					<img src="<?php echo base_url('assets/img/uq_logo.png'); ?>" class="uq-logo">
+				</div>
+				<ul class="nav navbar-right top-nav pull-right">
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i>&emsp;<?php echo $username;?> <b class="caret"></b></a>
+						<ul class="dropdown-menu">
+							<li>
+								<a href="#"><i class="fa fa-fw fa-user"></i>&emsp;Profile</a>
+							</li>
+							<li>
+								<a href="#"><i class="fa fa-fw fa-envelope"></i>&emsp;Inbox</a>
+							</li>
+							<li>
+								<a href="#"><i class="fa fa-fw fa-gear"></i>&emsp;Settings</a>
+							</li>
+							<li class="divider"></li>
+							<li>
+								<a href="<?php echo site_url('home/logout') ?>"><i class="fa fa-fw fa-power-off"></i>&emsp;Log Out</a>
+							</li>
+						</ul>
+					</li>
+				</ul>
+			</nav>
 			<div id="sidebar-wrapper">
 				<ul class="nav navbar-nav side-nav">
 					<li class="side-user hidden-xs">
@@ -64,31 +61,28 @@
 
 							<!-- Original User Login Ver. -->
 							<img class="img-circle" src="<?php echo base_url('assets/img/user-placeholder.jpg'); ?>" alt="User Placeholder Image" /> 
-							<!-- <p class="welcome">Logged in as</p> -->
-							<!-- <p class="name"><?php echo $username;?></p> -->
-							<p>
-								
-								<a href="#" class="name" data-toggle="dropdown"><?php echo $username;?> <b class="caret"></b></a>
-								<!-- <ul class="dropdown-menu">
-									<li>
-										<a href="#"><i class="fa fa-fw fa-user"></i>&emsp;Profile</a>
-									</li>
-									<li>
-										<a href="#"><i class="fa fa-fw fa-envelope"></i>&emsp;Inbox</a>
-									</li>
-									<li>
-										<a href="#"><i class="fa fa-fw fa-gear"></i>&emsp;Settings</a>
-									</li>
-									<li class="divider"></li>
-									<li>
-										<a href="<?php echo site_url('home/logout') ?>"><i class="fa fa-fw fa-power-off"></i>&emsp;Log Out</a>
-									</li>
-								</ul> -->
-							</p>
+							<p class="welcome">Logged in as</p>
+							<p class="name"><?php echo $username;?></p>
+							<!-- <a href="#" class="name" data-toggle="dropdown"><?php echo $username;?> <b class="caret"></b></a>
+							<ul class="dropdown-menu">
+								<li>
+									<a href="#"><i class="fa fa-fw fa-user"></i>&emsp;Profile</a>
+								</li>
+								<li>
+									<a href="#"><i class="fa fa-fw fa-envelope"></i>&emsp;Inbox</a>
+								</li>
+								<li>
+									<a href="#"><i class="fa fa-fw fa-gear"></i>&emsp;Settings</a>
+								</li>
+								<li class="divider"></li>
+								<li>
+									<a href="<?php echo site_url('home/logout') ?>"><i class="fa fa-fw fa-power-off"></i>&emsp;Log Out</a>
+								</li>
+							</ul> -->
 						</div>
 					</li>
-					
-					<li class="side-nav-search">
+
+					<!-- <li class="side-nav-search">
 						<div class="input-group custom-search-form">
 							<input type="text" class="form-control" placeholder="Search...">
 							<span class="input-group-btn">
@@ -97,7 +91,7 @@
 								</button>
 							</span>
 						</div>
-					</li>
+					</li> -->
 
 					<li class="<?php if ($menu=="home") { echo "active"; } ?>">
 						<a class="side-nav-font" href="<?php echo site_url('home') ?>"><i class="fa fa-fw fa-home"></i>&emsp;Home</a>
