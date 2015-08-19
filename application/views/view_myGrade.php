233a2bdfcb46d5f29e1864309a3e3a06f1b9ae12
Malcolm Joseland <malcolm.joseland@uqconnect.edu.au>
2015-08-20 01:39:02 +1000

    Made "My Grade" page mostly dynamic

--- application/views/view_myGrade.php
+++ application/views/view_myGrade.php
@@ -1,3 +1,19 @@
+<?php //$userID = 1; 
+
+$query = $this->db->query('SELECT courseID, completion, grading FROM user_courses WHERE userID = 2 AND enrolled = 1');
+$coursesQuery = $this->db->query('SELECT courseID, courseName FROM courses');
+
+/*foreach ($query->result() as $row)
+{
+    echo $row->courseID, "\r\n";
+    echo $row->completion,"\r\n";
+    echo $row->grading, "\r\n";
+}
+
+echo 'Total Results: ' . $query->num_rows();*/
+
+?>
+
 <div id="page-wrapper">
 	<div class="container-fluid">
 		<!-- Page Heading -->
@@ -9,19 +25,51 @@
 			</div>
 		</div>
 
-		<div class="row">
-			<div class="panel-body">
-				<div class="alert alert-success">
-					<h2><b>Phising Email</b></h2>
-					<div class="progress">
-						<div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="40"
-						aria-valuemin="0" aria-valuemax="100" style="width:100%">
-						Complete 
-					</div>
-				</div>
-				<h3>Score: 56</h3>
-			</div>
 
+		<div class=\"row\">
+		<div class=\"panel-body\">
+		<?php
+
+		foreach ($query->result() as $row) {
+			//find the name of the course
+			$courseName = "Course";
+			foreach ($coursesQuery->result() as $coursesRow) {
+				if ($coursesRow->courseID == $row->courseID) {
+					$courseName = $coursesRow->courseName;
+				}
+			}
+
+			//find the colour/style (depending on completion level)
+			$completion = $row->completion;
+			$style = "";
+			if ($completion == 100) {
+				$style = "success";
+			} else if ($completion == 0) {
+				$style = "danger";
+			} else {
+				$style = "warning";
+			}
+
+
+
+			echo "<div class=\"alert alert-$style\">";
+				echo "<h2><b> $courseName </b></h2>";
+					echo "<div class=\"progress\">";
+						echo "<div class=\"progress-bar progress-bar-$style\" role=\"progressbar\" aria-valuenow=\"40\"";
+							echo "aria-valuemin=\"0\" aria-valuemax=\"100\" style=\"width:$completion%\">";
+							if ($row->completion == 100) {
+								echo "Complete";
+							} else if ($row->completion > 0) {
+								echo "$row->completion%";
+							}
+						echo "</div>";
+					echo "</div>";
+				echo "<h3>Grade:$row->grading</h3>";
+			echo "</div>";
+		}
+		?>
+
+<?php /*
 			<div class="alert alert-info">
 				<h2><b>Choosing Password</b></h2>
 				<div class="progress">
@@ -53,6 +101,8 @@
 	</div>
 
 	<h3>Score: -</h3>
+
+*/ ?>
 
 </div>
 </div>
