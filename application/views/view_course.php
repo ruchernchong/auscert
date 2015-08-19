38728d02f103b595dac971c6947f9367b8e40fb8
Ru Chern <iruchern@gmail.com>
2015-08-19 16:15:02 +1000

    Temporarily removed email inbox in interactive quiz.

--- application/views/view_course.php
+++ application/views/view_course.php
@@ -107,7 +107,7 @@
 
 <!-- jQuery -->
 <script src="<?php echo base_url('assets/js/jquery-1.11.3.min.js'); ?>"></script>
-<script src="<?php echo base_url('assets/js/email.js'); ?>"></script>
+<!-- <script src="<?php echo base_url('assets/js/email.js'); ?>"></script> -->
 
 <!-- Bootstrap Core JavaScript -->
 <script src="<?php echo base_url('assets/js/bootstrap.min.js'); ?>"></script>
