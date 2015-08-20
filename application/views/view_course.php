341722abec382c0f96fdcda855856a56611f5a1b
Ru Chern <iruchern@gmail.com>
2015-08-20 22:45:25 +1000

    Disable clicking to navigate back to the same page

--- application/views/view_course.php
+++ application/views/view_course.php
@@ -108,6 +108,11 @@ $("#menu-toggle").click(function(e) {
 	e.preventDefault();
 	$("#wrapper").toggleClass("toggled");
 });
+
+$(document).ready(function() {
+	// $('[data-toggle="tooltip"]').tooltip();
+	$('#pageCourse').removeAttr('href');
+});
 </script>
 </body>
 </html>
