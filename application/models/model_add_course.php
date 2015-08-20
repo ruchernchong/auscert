85e5fd2993ac6592c001345c2cd53665388a682f
Cameron Paulsen <cameron.paulsen@uqconnect.edu.au>
2015-08-20 12:17:19 +1000

    Add course page now allows for a course to be created and saved to the database. User editable content and creator works correctly, date not yet implemented

---
+++ application/models/model_add_course.php
@@ -0,0 +1,58 @@
+<?php
+
+Class model_add_course extends CI_Model {
+	function __construct() {
+		parent::__construct();
+	}
+
+	 public function save_course($title, $category, $active, $description, $date_created, $last_edited) {
+	   
+
+	 	$data = array(
+               'courseName' => $title,
+               'category' => $category,
+               'creator' => $this->session->userdata['logged_in']['username'],
+               'active' =>  $active,
+               'description' => $description,
+               'dateCreated' => $date_created, 
+               'lastEdited' => $last_edited
+            );
+
+		$this->db->insert('courses', $data); 
+	 }
+
+	/*
+	public function validate() {
+		$this->db->where('username',$this->input->post('username'));
+		$this->db->where('password',$this->input->post('password'));
+
+		$query = $this->db->get('users');
+
+		if ($query->num_rows == 1) {
+			return $query->result();
+		}
+		return false;
+	}
+
+	public function GetUserCourses() {
+		// $this->db->select('*');
+		$this->db->where('userID', $this->session->userdata['logged_in']['userID']);
+		$this->db->join('courses', 'courses.courseID = user_courses.courseID', "INNER");
+		$this->db->order_by("courseName", "ASC");
+		$query = $this->db->get('user_courses');
+		return $query->result();
+	}
+
+	public function GetNumberOfUserCourses() {
+		$this->db->where('userID', $this->session->userdata['logged_in']['userID']);
+		$query = $this->db->get('user_courses');
+		return $query->num_rows;
+	}
+	*/
+}
+?>
+
+
+
+
+
