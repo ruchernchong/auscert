89a61dc172e27abca378e0cf781e9b0d9e2e9b92
Ru Chern <iruchern@gmail.com>
2015-08-20 13:57:07 +1000

    Fixed formatting

--- application/views/view_myGrade.php
+++ application/views/view_myGrade.php
@@ -26,48 +26,48 @@ echo 'Total Results: ' . $query->num_rows();*/
 		</div>
 
 
-		<div class=\"row\">
-		<div class=\"panel-body\">
-		<?php
+		<div class="row">
+			<div class="panel-body">
+				<?php
 
-		foreach ($query->result() as $row) {
+				foreach ($query->result() as $row) {
 			//find the name of the course
-			$courseName = "Course";
-			foreach ($coursesQuery->result() as $coursesRow) {
-				if ($coursesRow->courseID == $row->courseID) {
-					$courseName = $coursesRow->courseName;
-				}
-			}
+					$courseName = "Course";
+					foreach ($coursesQuery->result() as $coursesRow) {
+						if ($coursesRow->courseID == $row->courseID) {
+							$courseName = $coursesRow->courseName;
+						}
+					}
 
 			//find the colour/style (depending on completion level)
-			$completion = $row->completion;
-			$style = "";
-			if ($completion == 100) {
-				$style = "success";
-			} else if ($completion == 0) {
-				$style = "danger";
-			} else {
-				$style = "warning";
-			}
+					$completion = $row->completion;
+					$style = "";
+					if ($completion == 100) {
+						$style = "success";
+					} else if ($completion == 0) {
+						$style = "danger";
+					} else {
+						$style = "warning";
+					}
 
 
 
-			echo "<div class=\"alert alert-$style\">";
-				echo "<h2><b> $courseName </b></h2>";
+					echo "<div class=\"alert alert-$style\">";
+					echo "<h2><b> $courseName </b></h2>";
 					echo "<div class=\"progress\">";
-						echo "<div class=\"progress-bar progress-bar-$style\" role=\"progressbar\" aria-valuenow=\"40\"";
-							echo "aria-valuemin=\"0\" aria-valuemax=\"100\" style=\"width:$completion%\">";
-							if ($row->completion == 100) {
-								echo "Complete";
-							} else if ($row->completion > 0) {
-								echo "$row->completion%";
-							}
-						echo "</div>";
+					echo "<div class=\"progress-bar progress-bar-$style\" role=\"progressbar\" aria-valuenow=\"40\"";
+					echo "aria-valuemin=\"0\" aria-valuemax=\"100\" style=\"width:$completion%\">";
+					if ($row->completion == 100) {
+						echo "Complete";
+					} else if ($row->completion > 0) {
+						echo "$row->completion%";
+					}
 					echo "</div>";
-				echo "<h3>Grade:$row->grading</h3>";
-			echo "</div>";
-		}
-		?>
+					echo "</div>";
+					echo "<h3>Grade:$row->grading</h3>";
+					echo "</div>";
+				}
+				?>
 
 <?php /*
 			<div class="alert alert-info">
