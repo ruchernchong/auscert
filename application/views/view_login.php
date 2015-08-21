feccdfb32dca03ae6d47f5e103e6afd39f775796
Ru Chern <iruchern@gmail.com>
2015-08-21 12:33:43 +1000

    Added Register button

--- application/views/view_login.php
+++ application/views/view_login.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>My Place Login</title>
+	<title>AusCert | Login</title>
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/style.css'); ?>" />
 	<!-- <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/bootstrap.min.css'); ?>" /> -->
 	<script src="<?php echo base_url('assets/js/jquery-1.11.3.min.js'); ?>"></script>
@@ -13,10 +13,7 @@
 <body class="login_bg">
 	<div class="uq-login">
 		<div class="uq-logo"></div>
-		<form method="post" class="login_form effect7" action='login/validateLogin' name='Check_Login'>
-			<?php
-			$attr = array('class' => 'login_form effect7');
-			?>
+		<form name='formLogin' class="formLogin effect7" action='login/validateLogin' method="post">
 			<div class="form-group">
 				<label for="username" class="login_icon icon-person"></label>
 				<input type="text" id="username" name="username" class="text-input" />
@@ -26,9 +23,18 @@
 				<input type="password" id="password" name="password" class="text-input" />
 			</div>
 			<div class="form-group">
-				<input type="submit" id="submit" value="Log In" name="submit" class="btn btn-uq" />
+				<input type="submit" id="submit" name="submit" class="btn-uq" value="Log In"  />
+			</div>
+			<div class="form-group">
+				<button type="button" class="btn-uq" id="btnRegister">Register</button>
 			</div>
 		</form>
 	</div>
+	<script>
+	$("#btnRegister").click(function(e) {
+		e.preventDefault();
+		window.location.href = "register";
+	});
+	</script>
 </body>
 </html>
