9e535e58c177627eac0ea513cb441c0b58dd1921
Ru Chern <iruchern@gmail.com>
2015-08-23 15:01:15 +1000

    Disable mobile menu while on desktop

--- application/views/header.php
+++ application/views/header.php
@@ -48,7 +48,7 @@
 				</div>
 
 				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-					<ul class="nav navbar-nav pull-right">
+					<ul class="nav navbar-nav pull-right" id="dropdownMenu">
 						<li class="dropdown">
 							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
 								<i class="fa fa-user"></i>&emsp;<?php echo $username;?>
