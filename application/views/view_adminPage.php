72536badc7cf6764bb8ce0d605c7c62435763e58
Ru Chern <iruchern@gmail.com>
2015-09-01 13:36:14 +1000

    Resize Search bar

--- application/views/view_adminPage.php
+++ application/views/view_adminPage.php
@@ -11,73 +11,83 @@
 				Last modified: <i class="fa fa-clock-o"></i>&emsp;<?php echo $courseLastEdited[0]->courseName . "; " . $courseLastEdited[0]->lastEdited; ?>
 
 				<h2>Report</h2>
-				<div class="input-group">
-					<input type="text" placeholder="Search client" class="input form-control">
-					<span class="input-group-btn">
-						<button type="button" class="btn btn btn-primary">
-							<i class="fa fa-search"></i>&emsp;Search</button>
-						</span>
+				<div class="row">
+					<div class="col-lg-8">
 					</div>
-					<div class="clients-list">
-						<ul class="nav nav-tabs">
-							<li class="active">
-								<a data-toggle="tab" href="#tab-courses"><i class="fa fa-briefcase"></i>&emsp;Courses</a>
-							</li>
-							<li>
-								<a data-toggle="tab" href="#tab-members"><i class="fa fa-user"></i>&emsp;Members</a>
-							</li>
-						</ul>
+					<div class="col-lg-4">
+						<div class="input-group">
+							<div class="input-group-btn">
+							</div>
+							<input type="search" placeholder="Search" class="form-control">
+							<span class="input-group-btn">
+								<button type="button" class="btn btn-primary">
+									<i class="fa fa-search"></i>
+								</button>
+							</span>
+						</div>
+					</div>
+				</div>
+				<div class="clients-list">
+					<ul class="nav nav-tabs">
+						<li class="active">
+							<a data-toggle="tab" href="#tab-courses"><i class="fa fa-briefcase"></i>&emsp;Courses</a>
+						</li>
+						<li>
+							<a data-toggle="tab" href="#tab-members"><i class="fa fa-user"></i>&emsp;Members</a>
+						</li>
+					</ul>
 
-						<div class="tab-content">
-							<div id="tab-members" class="tab-pane fade">
-								<div class="table-responsive">
-									<table class="table table-striped table-hover">
-										<thead>
-											<tr>
-												<th>Username</th>
-												<th>User Type</th>
-												<th>Email Address</th>
-												<th>Contact No.</th>
-											</tr>
-										</thead>
-										<tbody>
-											<?php foreach ($users as $user) { ?>
-											<tr>
-												<td class="client-avatar">
-													<img alt="image" src="<?php echo base_url('assets/img/user-placeholder.jpg'); ?>">&emsp;<a data-toggle="tab" href="#<?php echo $user->username; ?>" class="client-link"><?php echo $user->username; ?></a>
-												</td>
-												<td>
-													<span data-toggle="tooltip" title="Any suggestion what would you prefer for this? Right now I am using 'userType' from the database."><?php echo $user->userType; ?></span>
-												</td>
-												<td>
-													<i class="fa fa-envelope"></i>&emsp;<a href="mailto:<?php echo $user->email; ?>"><?php echo $user->email; ?></a>
-												</td>
-												<td>
-													<i class="fa fa-phone"></i>&emsp;<a href="tel:<?php echo $user->contact; ?>"><?php echo $user->contact; ?></a>
-												</td>
+					<div class="tab-content">
+						<div id="tab-members" class="tab-pane fade">
+							<div class="table-responsive">
+								<table class="table table-striped table-hover">
+									<thead>
+										<tr>
+											<th>Username</th>
+											<th>User Type</th>
+											<th>Email Address</th>
+											<th>Contact No.</th>
+										</tr>
+									</thead>
+									<tbody>
+										<?php foreach ($users as $user) { ?>
+										<tr>
+											<td class="client-avatar">
+												<img alt="image" src="<?php echo base_url('assets/img/user-placeholder.jpg'); ?>">&emsp;<a data-toggle="tab" href="#<?php echo $user->username; ?>" class="client-link"><?php echo $user->username; ?></a>
+											</td>
+											<td>
+												<span data-toggle="tooltip" title="Any suggestion what would you prefer for this? Right now I am using 'userType' from the database."><?php echo $user->userType; ?></span>
+											</td>
+											<td>
+												<i class="fa fa-envelope"></i>&emsp;<a href="mailto:<?php echo $user->email; ?>"><?php echo $user->email; ?></a>
+											</td>
+											<td>
+												<i class="fa fa-phone"></i>&emsp;<a href="tel:<?php echo $user->contact; ?>"><?php echo $user->contact; ?></a>
+											</td>
 												<!-- <td class="client-status">
 													<span class="label label-success" data-toggle="tooltip" title="I have no idea what is this.">Complete All Task</span>
 												</td> -->
 											</tr>
 											<?php
-											}
-											?>
-										</tbody>
-									</table>
-								</div>
+										}
+										?>
+									</tbody>
+								</table>
 							</div>
+						</div>
 
-							<div id="tab-courses" class="tab-pane fade in active">
-								<div class="table-responsive">
-									<table class="table table-hover">
-										<thead>
-											<th>Course List</th>
-											<th>Last Edited</th>
-											<th>Status</th>
-											<th>Actions</th>
-										</thead>
-										<tbody>
-											<?php foreach ($courses as $course) { ?>
+						<div id="tab-courses" class="tab-pane fade in active">
+							<div class="table-responsive">
+								<table class="table table-hover">
+									<thead>
+										<th>Course List</th>
+										<th>Last Edited</th>
+										<th>Status</th>
+										<th>Actions</th>
+									</thead>
+									<tbody>
+										<?php foreach ($courses as $course) { 
+											?>
 											<tr>
 												<td><a href="<?php echo site_url('learning?courseID=' . $course->courseID); ?>"><?php echo $course->courseName; ?></a></td>
 												<td><?php echo empty($course->lastEdited) ? "None" : $course->lastEdited; ?></td>
