f65f9d6423328baf12740c8dd116401c6a0e9fc5
Ru Chern <iruchern@gmail.com>
2015-09-01 13:39:30 +1000

    Added if else statement to throw "No users" message.

--- application/views/view_addCourse.php
+++ application/views/view_addCourse.php
@@ -10,28 +10,35 @@
 
 		<div class="row">	
 			<div class="col-lg-12">
-				<div class="form-group">
-					<form name ="userInput" id="userInput" action="addCourse/createCourse" method="post">
-						<label for="courseName">Course Name</label>
-						<input class="form-control" name="courseName" required>
-						<p class="help-block">Must be filled</p>
+				<div class="panel panel-default">
+					<!-- <div class="panel panel-heading">
+						<h4 class="panel-title">Add Course</h4>
+					</div> -->
+					<div class="panel panel-body">
+						<div class="form-group">
+							<form name ="userInput" id="userInput" action="addCourse/createCourse" method="post">
+								<label for="courseName">Course Name</label>
+								<input class="form-control" name="courseName" required>
+								<p class="help-block">Must be filled</p>
 
-						<label for="courseCategory">Category</label>
-						<select class="form-control" name="courseCategory" required>
-							<!-- Example options. Will change to dynamic. -->
-							<option value="Introductory">Introductory Courses</option>
-							<option value="Safety">Safety</option>
-							<option value="Security">Security</option>
-						</select>
-						<p class="help-block">Must be filled</p>
+								<label for="courseCategory">Category</label>
+								<select class="form-control" name="courseCategory" required>
+									<!-- Example options. Will change to dynamic. -->
+									<option value="Introductory">Introductory Courses</option>
+									<option value="Safety">Safety</option>
+									<option value="Security">Security</option>
+								</select>
+								<p class="help-block">Must be filled</p>
 
-						<label for="courseDescription">Description</label>
-						<textarea class="form-control" rows="5" name="courseDescription" required></textarea>
-						<p class="help-block">Must be filled</p>
+								<label for="courseDescription">Description</label>
+								<textarea class="form-control" rows="5" name="courseDescription" required></textarea>
+								<p class="help-block">Must be filled</p>
 
-						<input type="submit" class="btn btn-success" value="Submit" />
-						<button type="reset" class="btn btn-danger">Reset</button>
-					</form>
+								<input type="submit" class="btn btn-success" value="Submit" />
+								<button type="reset" class="btn btn-danger">Reset</button>
+							</form>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
--- application/views/view_analysis.php
+++ application/views/view_analysis.php
@@ -1,88 +1,111 @@
 <div id="page-wrapper">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-lg-12">
-                <h1 class="page-header">Course Analysis</h1>
-                <h4><?php echo $course->courseName ?></h4>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-lg-12">
+				<h1 class="page-header">Course Analysis
+					<small>Data, and more Data!</small>
+				</h1>
 
-                <h3>Users Enrolled</h3>
-                <table class="table table-striped table-condensed">
-                    <thead>
-                        <tr>
-                            <th>UserID</th>
-                            <th>User Name</th>
-                            <th>Completion</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    <?php
-                    if (is_array($courseUsers)) {
-                        foreach ($courseUsers as $courseUser) { ?>
-                            <tr>
-                                <td><?php echo $courseUser->userID ?></td>
-                                <td><?php echo $courseUser->username ?></td>
-                                <td><?php echo $courseUser->completion ?></td>
-                            </tr>
-                            <?php
-                        };
-                    } else {
-                        echo "<tr><td>" . $courseUsers . "</td></tr>";
-                    }
-                    ?>
-                    </tbody>
-                </table>
+				<div class="panel panel-info">
+					<div class="panel panel-heading">
+						<h2><?php echo $course->courseName ?></h2>
+					</div>
+					<div class="panel panel-body">
+						<h4>Users Enrolled</h4>
+						<table class="table table-striped table-hover">
+							<thead>
+								<tr>
+									<th>UserID</th>
+									<th>Username</th>
+									<th>Completion</th>
+								</tr>
+							</thead>
+							<tbody>
+								<?php
+								if (empty($courseUsers)) {
+									?>
+									<tr>
+										<td colspan="3">There are no users enrolled in this course.</td>
+									</tr>
+									<?php
+								} else if (is_array($courseUsers)) {
+									foreach ($courseUsers as $courseUser) { 
+										?>
+										<tr>
+											<td><?php echo $courseUser->userID ?></td>
+											<td><?php echo $courseUser->username ?></td>
+											<td><?php echo $courseUser->completion ?></td>
+										</tr>
+										<?php
+									};
+								} else {
+									?>
+									<tr>
+										<td><?php echo $courseUsers; ?></td>
+									</tr>
+									<?php
+								}
+								?>
+							</tbody>
+						</table>
 
-                <h3>Users Completed</h3>
-                <table class="table table-striped table-condensed">
-                    <thead>
-                    <tr>
-                        <th>UserID</th>
-                        <th>User Name</th>
-                        <th>Score</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <?php
-                    if (is_array($completedUsers)) {
-                        foreach ($completedUsers as $completedUser) { ?>
-                            <tr>
-                                <td><?php echo $completedUser->userID ?></td>
-                                <td><?php echo $completedUser->username ?></td>
-                                <td><?php echo 'Score here' ?></td>
-                            </tr>
-                            <?php
-                        };
-                    } else {
-                        echo "<tr><td>" . $completedUsers . "</td></tr>";
-                    }
-                    ?>
-                    </tbody>
-                </table>
+						<h4>Users Completed</h4>
+						<table class="table table-striped table-hover">
+							<thead>
+								<tr>
+									<th>UserID</th>
+									<th>Username</th>
+									<th>Score</th>
+								</tr>
+							</thead>
+							<tbody>
+								<?php
+								if (empty($completedUsers)) {
+									?>
+									<tr>
+										<td colspan="3">No users have completed this course yet.</td>
+									</tr>
+									<?php
+								} else if (is_array($completedUsers)) {
+									foreach ($completedUsers as $completedUser) { 
+										?>
+										<tr>
+											<td><?php echo $completedUser->userID; ?></td>
+											<td><?php echo $completedUser->username; ?></td>
+											<td><?php echo $completedUser->grading; ?></td>
+										</tr>
+										<?php
+									};
+								} else {
+									?>
+									<tr>
+										<td><?php echo $completedUsers; ?></td>
+									</tr>
+									<?php
+								}
+								?>
+							</tbody>
+						</table>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-lg-12">
 
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-lg-12">
 
+					</div>
+				</div>
+			</div>
+		</div>
+		<script>
+		$("#menu-toggle").click(function(e) {
+			e.preventDefault();
+			$("#wrapper").toggleClass("toggled");
+		});
 
-            </div>
-        </div>
-    </div>
-</div>
-
-
-
-
-<script>
-    $("#menu-toggle").click(function(e) {
-        e.preventDefault();
-        $("#wrapper").toggleClass("toggled");
-    });
-
-    $(document).ready(function() {
-        $('[data-toggle="tooltip"]').tooltip();
-        $('#pageAdmin').removeAttr('href');
-    });
-</script>
-</body>
-</html>
+		$(document).ready(function() {
+	// $('[data-toggle="tooltip"]').tooltip();
+	$('#pageAdmin').removeAttr('href');
+});
+		</script>
+	</body>
+	</html>
