c254290f8b3f3b55c50d298f50ba00c8d0093063
Ru Chern <iruchern@gmail.com>
2015-09-08 18:55:52 +1000

    Add success and failure message.

--- sqlfiles/ImportDB.bat
+++ sqlfiles/ImportDB.bat
@@ -1,4 +1,14 @@
 @ECHO OFF
 
 mysql -u auscert -padmin auscertdb < auscertdb.sql
+
+if %errorlevel% == 0 goto success
+if %errorlevel% == 1 goto failure
+
+:success
+echo Database has been successfully imported.
+
+:failure
+echo Database has failed to import the file.
+
 pause
