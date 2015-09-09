aebed8e4e04a3d107a245433c5a933407eec9ace
Ru Chern <iruchern@gmail.com>
2015-09-10 01:58:50 +1000

    Dashboard UI Update v2.0 #23

--- application/views/header.php
+++ application/views/header.php
@@ -71,64 +71,45 @@
 			</div>
 		</nav>
 
-		<div id="sidebar-wrapper">
-			<ul class="nav navbar-nav side-nav">
-				<li class="side-user hidden-xs">
-					<div class="photo">
-						<img src="<?php echo base_url('assets/img/uq_logo.png'); ?>" class="uq-logo" alt="UQ Logo">
-						<img class="img-circle" src="<?php echo base_url('assets/img/user-placeholder.jpg'); ?>" alt="User Placeholder Image" /> 
-						<p class="welcome">Logged in as</p>
-						<p class="name"><?php echo $username;?></p>
-							<!-- <a href="#" class="name" data-toggle="dropdown"><?php echo $username;?> <b class="caret"></b></a>
-							<ul class="dropdown-menu">
-								<li>
-									<a href="#"><i class="fa fa-fw fa-user"></i>&emsp;Profile</a>
-								</li>
-								<li>
-									<a href="#"><i class="fa fa-fw fa-envelope"></i>&emsp;Inbox</a>
-								</li>
-								<li>
-									<a href="#"><i class="fa fa-fw fa-gear"></i>&emsp;Settings</a>
-								</li>
-								<li class="divider"></li>
-								<li>
-									<a href="<?php echo site_url('home/logout') ?>"><i class="fa fa-fw fa-power-off"></i>&emsp;Log Out</a>
-								</li>
-							</ul> -->
-						</div>
-					</li>
+		<!-- <div id="sidebar-wrapper">
+		<ul class="nav navbar-nav side-nav">
+			<li class="side-user hidden-xs">
+				<div class="photo">
+					<img src="<?php echo base_url('assets/img/uq_logo.png'); ?>" class="uq-logo" alt="UQ Logo">
+					<img class="img-circle" src="<?php echo base_url('assets/img/user-placeholder.jpg'); ?>" alt="User Placeholder Image" /> 
+					<p class="welcome">Logged in as</p>
+					<p class="name"><?php echo $username;?></p>
+				</div>
+			</li>
 
-					<li>
-						<a id="pageHome" class="side-nav <?php echo $menu=='home' ? 'active' : '' ?>" href="<?php echo site_url('home'); ?>">
-							<i class="fa fa-fw fa-home"></i>&emsp;Home
-						</a>
-					</li>
-<!-- 					<li class="<?php if ($menu=="mygrade") { echo "active"; } ?>">
-						<a class="side-nav" href="<?php echo site_url('home/mygrade') ?>"><i class="fa fa-fw fa-check-square"></i>&emsp;My Grade</a>
-					</li> -->
-					<li>
-						<a id="pageCourse" class="side-nav <?php echo $menu=='course' ? 'active' : '' ?>" href="<?php echo site_url('course'); ?>">
-							<i class="fa fa-fw fa-briefcase"></i>&emsp;Course
-						</a>
-					</li>
-					<?php if ($usertype=="admin") { ?>
-					<li>
-						<a id="pageAdmin" class="side-nav <?php echo $menu=='admin' ? 'active' : '' ?>" href="<?php echo site_url('admin'); ?>">
-							<i class="fa fa-fw fa-folder-open"></i>&emsp;Admin Page
-						</a>	
-					</li>
-					<?php 
-				} 
-				?>
-				<li>
-					<a id="pageSettings" class="side-nav <?php echo $menu=='settings' ? 'active' : '' ?>" href="<?php echo site_url('settings'); ?>">
-						<i class="fa fa-fw fa-gear"></i>&emsp;Settings
-					</a>
-				</li>
-				<li>
-					<a id="pageLogout" class="side-nav <?php echo $menu=='logout' ? 'active' : '' ?>" href="<?php echo site_url('logout'); ?>">
-						<i class="fa fa-fw fa-power-off"></i>&emsp;Log Out
-					</a>
-				</li>
-			</ul>
-		</div>
+			<li>
+				<a id="pageHome" class="side-nav <?php echo $menu=='home' ? 'active' : '' ?>" href="<?php echo site_url('home'); ?>">
+					<i class="fa fa-fw fa-home"></i>&emsp;Home
+				</a>
+			</li>
+			<li>
+				<a id="pageCourse" class="side-nav <?php echo $menu=='course' ? 'active' : '' ?>" href="<?php echo site_url('course'); ?>">
+					<i class="fa fa-fw fa-briefcase"></i>&emsp;Course
+				</a>
+			</li>
+			<?php if ($usertype=="admin") { ?>
+			<li>
+				<a id="pageAdmin" class="side-nav <?php echo $menu=='admin' ? 'active' : '' ?>" href="<?php echo site_url('admin'); ?>">
+					<i class="fa fa-fw fa-folder-open"></i>&emsp;Admin Page
+				</a>	
+			</li>
+			<?php 
+		} 
+		?>
+		<li>
+			<a id="pageSettings" class="side-nav <?php echo $menu=='settings' ? 'active' : '' ?>" href="<?php echo site_url('settings'); ?>">
+				<i class="fa fa-fw fa-gear"></i>&emsp;Settings
+			</a>
+		</li>
+		<li>
+			<a id="pageLogout" class="side-nav <?php echo $menu=='logout' ? 'active' : '' ?>" href="<?php echo site_url('logout'); ?>">
+				<i class="fa fa-fw fa-power-off"></i>&emsp;Log Out
+			</a>
+		</li>
+	</ul>
+</div> -->
