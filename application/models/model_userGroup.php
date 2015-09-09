89cb8c9eea8bd4b099bb248243d27b498ff3bbd8
xenarax <leonxenarax@gmail.com>
2015-09-09 20:04:27 +1000

    renamed model_userGroup to add a .php extension

---
+++ application/models/model_userGroup.php
@@ -0,0 +1,34 @@
+<?php
+
+Class model_userGroup extends CI_Model {
+    function __construct() {
+        parent::__construct();
+    }
+
+    //get a list of users details and the groups they are assigned to
+    public function GetUsersAndGroups() {
+        $this->db->join('groups', 'groups.groupID = user_groups.groupID', 'full outer');
+        $this->db->join('users', 'users.userID = user_groups.userID', 'full outer');
+        $query = $this->db->get('user_groups');
+
+        if ($query->num_rows >= 1) {
+            return $query->result();
+        }
+        return false;
+    }
+
+    //returns an array (instead of an object) of all groupID and organisations of a user
+    public function GetUserGroup($userID) {
+        $this->db->select('g.groupID, g.organisation');
+        $this->db->from('groups AS g, user_groups AS ug');
+        $this->db->where('ug.userID', $userID);
+        $this->db->where('g.groupID = ug.groupID');
+        $query = $this->db->get();
+
+        if ($query->num_rows >= 1) {
+            return $query->result_array();
+        }
+        return false;
+    }
+}
+?>
