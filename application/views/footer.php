ec776501e7aaf7600e55dd8e4a8adffa037496fd
Ru Chern <iruchern@gmail.com>
2015-09-10 14:11:22 +1000

    Revert "Added footer"
    This reverts commit 012dbba3cb0d4c39c636793c047c1e17649f50f8.

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,9 +1,14 @@
-<footer class="footer" style="width:100%;background-color:#49075e;height:71px;position:absolute;bottom:0;text-align:center;">
+<footer class="footer" style="width:100%;z-index:1100;background-color:#49075e;height:71px;position:fixed;bottom:0;text-align:center;">
 	<div class="container">
 		<p class="text-muted" style="margin-top:15px;color:white;">Tartiner Studios&reg; <?php echo date("Y"); ?></p>
 	</div>
 </footer>
 
+<!-- jQuery -->
+<script src="<?php echo base_url('assets/js/jquery-1.11.3.min.js'); ?>"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="<?php echo base_url('assets/js/bootstrap.min.js'); ?>"></script>
 <script>
 $("#menu-toggle").click(function(e) {
 	e.preventDefault();
