feee4e364d992a09386344b4fede29f8942171ed
Ru Chern <iruchern@gmail.com>
2015-09-10 20:09:32 +1000

    Found a fix for model.

--- application/models/model_groupCourse.php
+++ application/models/model_groupCourse.php
@@ -1,21 +1,21 @@
 <?php
 
 Class model_groupCourse extends CI_Model {
-    function __construct() {
-        parent::__construct();
-    }
+	function __construct() {
+		parent::__construct();
+	}
 
-    public function GetGroupCourses($groupID) {
-        $this->db->select('gc.groupID, gc.courseID, c.courseName');
-        $this->db->from('courses AS c, group_courses AS gc');
-        $where = "gc.courseID = c.courseID AND gc.groupID='" . $groupID . "'";
-        $this->db->where($where);
-        $query = $this->db->get();
+	public function GetGroupCourses($groupID) {
+		$this->db->select('gc.groupID, gc.courseID, c.courseName');
+		$this->db->from('courses AS c, group_courses AS gc');
+		$this->db->where('gc.groupID', $groupID);
+		$this->db->where('gc.courseID = c.courseID');
+		$query = $this->db->get();
 
-        if ($query->num_rows > 0) {
-            return $query->result();
-        }
-        return false;
-    }
+		if ($query->num_rows > 0) {
+			return $query->result();
+		}
+		return false;
+	}
 }
 ?>
