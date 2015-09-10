ce1ee129fac05fd844fbecd035e84b39e3d7ba5a
xenarax <leonxenarax@gmail.com>
2015-09-10 18:02:46 +1000

    Added table to group course view

--- application/models/model_groupCourse.php
+++ application/models/model_groupCourse.php
@@ -7,13 +7,14 @@ Class model_groupCourse extends CI_Model {
 
     //returns an array (instead of an object) of all groupID and organisations of a user
     public function GetGroupCourses($groupID) {
+        $this->db->distinct();
         $this->db->select('gc.groupID, c.courseID, c.courseName');
         $this->db->from('courses AS c, group_courses AS gc');
         $this->db->where('gc.groupID', $groupID);
         $query = $this->db->get();
 
         if ($query->num_rows > 0) {
-            return $query->result_array();
+            return $query->result();
         }
         return false;
     }
--- application/views/view_manageCourse.php
+++ application/views/view_manageCourse.php
@@ -9,22 +9,26 @@
 		</div>
 
         <div class="row">
-            <table class="table-responsive">
-                <thead>
-                    <tr>
-                        <th>Course ID</th>
-                        <th>Course Name</th>
-                    </tr>
-                </thead>
-                <tbody>
-                <?php foreach($assignedCourses as $course) { ?>
-                    <tr>
-                        <td><?php echo $course->courseID ?></td>
-                        <td><?php echo $course->courseName ?></td>
-                    </tr>
-                <?php } ?>
-                </tbody>
-            </table>
+            <div class="col-lg-12">
+                <div class="table-responsive">
+                    <table class="table table-striped table-hover">
+                        <thead>
+                            <tr>
+                                <th>Course ID</th>
+                                <th>Course Name</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        <?php foreach($assignedCourses as $course) {?>
+                            <tr>
+                                <td><?php echo $course->courseID ?></td>
+                                <td><?php echo $course->courseName ?></td>
+                            </tr>
+                        <?php } ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
 	</div>
 </div>
