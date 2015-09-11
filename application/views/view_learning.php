2fe2b7a9cfae4b79d1f7aa67446ed2c2ba241888
Ru Chern <iruchern@gmail.com>
2015-09-11 21:41:25 +1000

    Fix format and indentation for MVP code submission.

--- application/views/view_learning.php
+++ application/views/view_learning.php
@@ -19,11 +19,9 @@
 							<a href="#chapter_<?php echo $i; ?>" data-toggle="tab">
 								<i class="fa fa-book"></i>&emsp;<?php echo $i+1 . " &mdash; " . $slides[$i]->slideTitle; ?>
 							</a>
-							<span>
-								<i class="fa fa-times"></i>
-							</span>
+							<span><i class="fa fa-times"></i></span>
 						</li>
-					<?php
+						<?php
 					} 
 					?>
 					<li>
@@ -36,52 +34,75 @@
 						<div class="col-lg-12">
 							<div class="tab-content">
 								<div class="tab-pane fade in active" id="course_details">
-									<h3>Category:&emsp;<?php echo $course->category; ?></h3><br />
-									<p><?php echo $course->description; ?></p>
+									<h3>
+										Category:&emsp;<?php echo $course->category; ?>
+									</h3>
+									<p>
+										<?php echo $course->description; ?>
+									</p>
 								</div>
-								<?php for ($i=0; $i < sizeof($slides); $i++) { ?>
+								<?php 
+								for ($i=0; $i < sizeof($slides); $i++) {
+									?>
 									<div class="tab-pane fade" id="chapter_<?php echo $i; ?>">
-										<h2><?php echo $slides[$i]->slideTitle; ?></h2><br />
-										<p><?php echo $slides[$i]->slideContent ?><p>
-										
+										<h2><?php echo $slides[$i]->slideTitle; ?></h2>
+										<p>
+											<?php echo $slides[$i]->slideContent ?>
+										</p>
 									</div>
-
-								<?php } ?>
+									<?php
+								}
+								?>
 								<div class="tab-pane fade" id="course_quiz">
 									<div class="row display-table">
 										<div class="col-md-1 display-cell">
 											<i id="prev" class="fa fa-chevron-circle-left fa-5x" style="color:#bbb"></i>
 										</div>
 										<div class="col-md-10">
-										<?php 
+											<?php 
 											$attributes = array(
 												'id' => 'userInput',
 												'name' => 'userInput'
 												);
-
 											echo form_open('learning/quiz?courseID=' . $course->courseID, $attributes);
-											for($i=0; $i < sizeof($questions); $i++) {
-												if($i == 0) {
-													echo '<div class="collapse in" id="current_question">'; 
-												} else {
-													echo '<div class="collapse">'; 
+
+											for ($i=0; $i < sizeof($questions); $i++) {
+												if ($i == 0) {
+													?>
+													<div class="collapse in" id="current_question">
+														<?php
+													} else {
+														?>
+														<div class="collapse">
+															<?php
+														}
+														?>
+														<h3>Question <?php echo $i+1; ?></h3>
+														<p>
+															<?php echo $questions[$i]->questionText; ?>
+														</p>
+														<ul>
+															<?php
+															for ($j=1; $j < sizeof($answers[$i]); $j++) {
+																?>
+																<li>
+																	<input type="radio" name="<?php echo 'q' . $i; ?>" value="<?php echo $j; ?>" required/>&emsp;<?php echo $answers[$i][$j]->answerText; ?>
+																</li>
+																<?php
+															}
+															?>
+														</ul>
+													</div>
+													<?php
 												}
-											?>
-												<h3>Question <?php echo $i+1; ?></h3><br>
-												<?php echo $questions[$i]->questionText; ?><br>
-												<ul>
-													<?php for ($j=0; $j < sizeof($answers[$i]); $j++) { ?>
-														<li><input type="radio" name="<?php echo 'q' . $i; ?>" value="<?php echo $j; ?>" required/>&emsp;<?php echo $answers[$i][$j]->answerText; ?></li>
-													<?php }	?>
-												</ul>
+												?>
 											</div>
-											<?php }	?>
+											<div class="col-md-1 display-cell">
+												<i id="next" class="fa fa-chevron-circle-right fa-5x " style="color:#bbb"></i>
+											</div>
 										</div>
-										<div class="col-md-1 display-cell">
-											<i id="next" class="fa fa-chevron-circle-right fa-5x " style="color:#bbb"></i>
-										</div>
-									</div>
-								</form>
+									</form>
+								</div>
 							</div>
 						</div>
 					</div>
@@ -89,15 +110,12 @@
 			</div>
 		</div>
 	</div>
-</div>
-<script src="<?php echo base_url('assets/js/main.js'); ?>"></script>
-<script>
-
-$("#menu-toggle").click(function(e) {
-	e.preventDefault();
-	$("#wrapper").toggleClass("toggled");
-}); 
-
-</script>
+	<script src="<?php echo base_url('assets/js/main.js'); ?>"></script>
+	<script>
+	$("#menu-toggle").click(function(e) {
+		e.preventDefault();
+		$("#wrapper").toggleClass("toggled");
+	}); 
+	</script>
 </body>
 </html>
