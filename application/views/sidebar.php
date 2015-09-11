773bbac8720d0715e0947290f647ecfdc6eac134
Ru Chern <iruchern@gmail.com>
2015-09-11 20:50:26 +1000

    Convert site_url to base_url

--- application/views/sidebar.php
+++ application/views/sidebar.php
@@ -10,33 +10,35 @@
 		</li>
 
 		<li>
-			<a id="pageHome" class="side-nav <?php echo $menu=='home' ? 'active' : '' ?>" href="<?php echo site_url('home'); ?>">
+			<a id="pageHome" class="side-nav <?php echo $menu=='home' ? 'active' : '' ?>" href="<?php echo base_url('home'); ?>">
 				<i class="fa fa-fw fa-home"></i>&emsp;Home
 			</a>
 		</li>
 		<li>
-			<a id="pageCourse" class="side-nav <?php echo $menu=='course' ? 'active' : '' ?>" href="<?php echo site_url('course'); ?>">
+			<a id="pageCourse" class="side-nav <?php echo $menu=='course' ? 'active' : '' ?>" href="<?php echo base_url('course'); ?>">
 				<i class="fa fa-fw fa-briefcase"></i>&emsp;Course
 			</a>
 		</li>
-		<?php if ($usertype=="admin") { ?>
-		<li>
-			<a id="pageAdmin" class="side-nav <?php echo $menu=='admin' ? 'active' : '' ?>" href="<?php echo site_url('admin'); ?>">
-				<i class="fa fa-fw fa-folder-open"></i>&emsp;Admin Page
-			</a>	
-		</li>
 		<?php 
-	} 
-	?>
-	<li>
-		<a id="pageSettings" class="side-nav <?php echo $menu=='settings' ? 'active' : '' ?>" href="<?php echo site_url('settings'); ?>">
-			<i class="fa fa-fw fa-gear"></i>&emsp;Settings
-		</a>
-	</li>
-	<li>
-		<a id="pageLogout" class="side-nav <?php echo $menu=='logout' ? 'active' : '' ?>" href="<?php echo site_url('logout'); ?>">
-			<i class="fa fa-fw fa-power-off"></i>&emsp;Log Out
-		</a>
-	</li>
-</ul>
+		if ($usertype=="admin") {
+			?>
+			<li>
+				<a id="pageAdmin" class="side-nav <?php echo $menu=='admin' ? 'active' : '' ?>" href="<?php echo base_url('admin'); ?>">
+					<i class="fa fa-fw fa-folder-open"></i>&emsp;Admin Page
+				</a>
+			</li>
+			<?php 
+		} 
+		?>
+		<li>
+			<a id="pageSettings" class="side-nav <?php echo $menu=='settings' ? 'active' : '' ?>" href="<?php echo base_url('settings'); ?>">
+				<i class="fa fa-fw fa-gear"></i>&emsp;Settings
+			</a>
+		</li>
+		<li>
+			<a id="pageLogout" class="side-nav <?php echo $menu=='logout' ? 'active' : '' ?>" href="<?php echo base_url('logout'); ?>">
+				<i class="fa fa-fw fa-power-off"></i>&emsp;Log Out
+			</a>
+		</li>
+	</ul>
 </div>
