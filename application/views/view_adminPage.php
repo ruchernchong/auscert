e3de18d588882beed5cc7802dcec14f973af383c
Ru Chern <iruchern@gmail.com>
2015-09-12 19:38:06 +1000

    Issue with linking.

--- application/views/view_adminPage.php
+++ application/views/view_adminPage.php
@@ -58,26 +58,26 @@
 												<img alt="image" src="<?php echo base_url('assets/img/user-placeholder.jpg'); ?>">&emsp;<a data-toggle="tab" href="#<?php echo $user['userName']; ?>" class="client-link"><?php echo $user['userName']; ?></a>
 											</td>
 											<td>
-											<?php
-											$userArrays = $user['groupArray'];
-											if (!empty($userArrays)) {
-												foreach ($userArrays as $userArray) {
-											?>
-												<ul>
-													<li>
-														<a href="#"><?php echo $userArray['organisation']; ?></a>
-													</li>
-												</ul>
-											<?php
+												<?php
+												$userArrays = $user['groupArray'];
+												if (!empty($userArrays)) {
+													foreach ($userArrays as $userArray) {
+														?>
+														<ul>
+															<li>
+																<a href="#"><?php echo $userArray['organisation']; ?></a>
+															</li>
+														</ul>
+														<?php
+													}
+												} else {
+													?>
+													<ul>
+														<li>User does not belong to any group(s)</li>
+													</ul>
+													<?php
 												}
-											} else {
-											?>
-												<ul>
-													<li>User does not belong to any group(s)</li>
-												</ul>
-											<?php
-											}
-											?>
+												?>
 											</td>
 											<td>
 												<span data-toggle="tooltip" title="Any suggestion what would you prefer for this? Right now I am using 'userType' from the database."><?php echo $user['userType']; ?></span>
@@ -88,109 +88,106 @@
 											<td>
 												<i class="fa fa-phone"></i>&emsp;<a href="tel:<?php echo $user['contact']; ?>"><?php echo $user['contact']; ?></a>
 											</td>
-													<!-- <td class="client-status">
-														<span class="label label-success" data-toggle="tooltip" title="I have no idea what is this.">Complete All Task</span>
-													</td> -->
-												</tr>
-												<?php
-											}
-											?>
-									</tbody>
-								</table>
-							</div>
-						</div>
-
-						<div id="tab-courses" class="tab-pane fade in active">
-							<div class="table-responsive">
-								<table class="table table-hover">
-									<thead>
-										<th>Course List</th>
-										<th>Last Edited</th>
-										<th>Status</th>
-										<th>Actions</th>
-									</thead>
-									<tbody>
-										<?php foreach ($courses as $course) {
-											?>
-											<tr>
-												<td><a href="<?php echo site_url('learning?courseID=' . $course->courseID); ?>"><?php echo $course->courseName; ?></a></td>
-												<td><?php echo empty($course->lastEdited) ? "None" : $course->lastEdited; ?></td>
-												<td>
-													<?php
-													if ($course->active == 1) {
-														?>
-														<div class="btn btn-sm btn-default">
-															<input type="checkbox" id="activeChecked_<?php echo $course->courseID; ?>" class="courseActive" checked>
-															<label for="activeChecked_<?php echo $course->courseID; ?>" class="activeLabel">Active</label>
-														</div>
-														<?php
-													} else {
-														?>
-														<div class="btn btn-sm btn-default">
-															<input type="checkbox" id="activeNotChecked_<?php echo $course->courseID; ?>" class="courseActive">
-															<label for="activeNotChecked_<?php echo $course->courseID; ?>" class="activeLabel">Active</label>
-														</div>
-														<?php
-													}
-													?>
-												</td>
-
-												<td class="project-actions">
-													<a href="<?php echo site_url('edits?courseID=' . $course->courseID); ?>" class="btn btn-sm btn-success">
-														<i class="fa fa-pencil"></i>&emsp;Edit
-													</a>
-													&nbsp;
-													<a href="<?php echo site_url('analysis?courseID=' . $course->courseID); ?>" class="btn btn-sm btn-primary">
-														<i class="fa fa-bar-chart-o"></i>&emsp;Course Analytics
-													</a>
-													&nbsp;
-													<a href="<?php echo site_url('admin/dropCourse?id=' . $course->courseID); ?>" class="btn btn-sm btn-danger">
-														<i class="fa fa-trash"></i>&emsp;Remove
-													</a>
-												</td>
-											</tr>
-											<?php
-										}
-										?>
-									</tbody>
-								</table>
-							</div>
-						</div>
-
-						<div id="tab-groups" class="tab-pane fade">
-							<div class="table-responsive">
-								<table class="table table-striped table-hover">
-									<thead>
-										<tr>
-											<th>Group Name</th>
-											<th>Total Members</th>
-											<th>Actions</th>
 										</tr>
-									</thead>
-									<tbody>
-									<?php foreach($groups as $group) {?>
+										<?php
+									}
+									?>
+								</tbody>
+							</table>
+						</div>
+					</div>
+
+					<div id="tab-courses" class="tab-pane fade in active">
+						<div class="table-responsive">
+							<table class="table table-hover">
+								<thead>
+									<th>Course List</th>
+									<th>Last Edited</th>
+									<th>Status</th>
+									<th>Actions</th>
+								</thead>
+								<tbody>
+									<?php foreach ($courses as $course) {
+										?>
 										<tr>
-											<td><?php echo $group['organisation'] ?></td>
-											<td><?php echo $group['userCount'] ?></td>
+											<td><a href="<?php echo site_url('learning?courseID=' . $course->courseID); ?>"><?php echo $course->courseName; ?></a></td>
+											<td><?php echo empty($course->lastEdited) ? "None" : $course->lastEdited; ?></td>
 											<td>
-												<a href="<?php echo base_url('manageMembers?groupID=' . $group['groupID']); ?>" class="btn btn-sm btn-success"><i class="fa fa-signal"></i>&emsp;Manage Members</a>
+												<?php
+												if ($course->active == 1) {
+													?>
+													<div class="btn btn-sm btn-default">
+														<input type="checkbox" id="activeChecked_<?php echo $course->courseID; ?>" class="courseActive" checked>
+														<label for="activeChecked_<?php echo $course->courseID; ?>" class="activeLabel">Active</label>
+													</div>
+													<?php
+												} else {
+													?>
+													<div class="btn btn-sm btn-default">
+														<input type="checkbox" id="activeNotChecked_<?php echo $course->courseID; ?>" class="courseActive">
+														<label for="activeNotChecked_<?php echo $course->courseID; ?>" class="activeLabel">Active</label>
+													</div>
+													<?php
+												}
+												?>
+											</td>
+
+											<td class="project-actions">
+												<a href="<?php echo site_url('edits?courseID=' . $course->courseID); ?>" class="btn btn-sm btn-success">
+													<i class="fa fa-pencil"></i>&emsp;Edit
+												</a>
 												&nbsp;
-												<a href="<?php echo base_url('manageCourse?groupID=' . $group['groupID']); ?>" class="btn btn-sm btn-primary"><i class="fa fa-refresh fa-spin"></i>&emsp;Manage Courses</a>
+												<a href="<?php echo site_url('analysis?courseID=' . $course->courseID); ?>" class="btn btn-sm btn-primary">
+													<i class="fa fa-bar-chart-o"></i>&emsp;Course Analytics
+												</a>
 												&nbsp;
-												<a class="btn btn-sm btn-danger"><i class="fa fa-trash"></i>&emsp;Delete Group</a>
-												&nbsp;
+												<a href="<?php echo site_url('admin/dropCourse?id=' . $course->courseID); ?>" class="btn btn-sm btn-danger">
+													<i class="fa fa-trash"></i>&emsp;Remove
+												</a>
 											</td>
 										</tr>
+										<?php
+									}
+									?>
+								</tbody>
+							</table>
+						</div>
+					</div>
+
+					<div id="tab-groups" class="tab-pane fade">
+						<div class="table-responsive">
+							<table class="table table-striped table-hover">
+								<thead>
+									<tr>
+										<th>Group Name</th>
+										<th>Total Members</th>
+										<th>Actions</th>
+									</tr>
+								</thead>
+								<tbody>
+									<?php foreach($groups as $group) {?>
+									<tr>
+										<td><?php echo $group['organisation'] ?></td>
+										<td><?php echo $group['userCount'] ?></td>
+										<td>
+											<a href="<?php echo base_url('manageMember?groupID=' . $group['groupID']); ?>" class="btn btn-sm btn-success"><i class="fa fa-signal"></i>&emsp;Manage Members</a>
+											&nbsp;
+											<a href="<?php echo base_url('manageCourse?groupID=' . $group['groupID']); ?>" class="btn btn-sm btn-primary"><i class="fa fa-refresh fa-spin"></i>&emsp;Manage Courses</a>
+											&nbsp;
+											<a class="btn btn-sm btn-danger"><i class="fa fa-trash"></i>&emsp;Delete Group</a>
+											&nbsp;
+										</td>
+									</tr>
 									<?php } ?>
-									</tbody>
-								</table>
-							</div>
+								</tbody>
+							</table>
 						</div>
 					</div>
 				</div>
 			</div>
 		</div>
 	</div>
+</div>
 </div>
 
 <script src="<?php echo base_url('assets/js/main.js'); ?>"></script>
