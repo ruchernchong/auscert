4eb4f67341503c2351a6ef5893e1d05840af4147
Ru Chern <iruchern@gmail.com>
2015-09-12 14:42:30 +1000

    Fixed redirecting problems for adding and removing group courses.

--- application/controllers/manageCourse.php
+++ application/controllers/manageCourse.php
@@ -30,8 +30,8 @@ class manageCourse extends CI_Controller {
 			if ($thisGroup) {
 				$data['thisGroup'] = $thisGroup;
 			} else {
-                $data['thisGroup'] = null;
-            }
+				$data['thisGroup'] = null;
+			}
 
 			if ($assignedCourses) {
 				$data['assignedCourses'] = $assignedCourses;
@@ -59,37 +59,36 @@ class manageCourse extends CI_Controller {
 	}
 
     //assign courses to a group
-    function addCourses() {
-        $coursesArray = $this->input->post('courseIDs');
-        $groupID = $this->input->post('groupID');
+	function addCourses() {
+		$coursesArray = $this->input->post('courseIDs');
+		$groupID = $this->input->post('groupID');
 
-        foreach ($coursesArray as $courseID) {
-            $this->model_groupCourse->AddCourseToGroup($courseID, $groupID);
-        }
+		foreach ($coursesArray as $courseID) {
+			$this->model_groupCourse->AddCourseToGroup($courseID, $groupID);
+		}
 //        $this->debug_to_console($_SERVER['REQUEST_URI']);
 //        header("Location: " . $_SERVER['REQUEST_URI']);
 //        header('Location: http://localhost/auscert/manageCourse?groupID=1');
-    }
+	}
 
     //remove courses from a group
-    function removeCourses() {
-        $coursesArray = $this->input->post('courseIDs');
-        $groupID = $this->input->post('groupID');
+	function removeCourses() {
+		$coursesArray = $this->input->post('courseIDs');
+		$groupID = $this->input->post('groupID');
 
-        foreach ($coursesArray as $courseID) {
-            $this->model_groupCourse->RemoveCourseFromGroup($courseID, $groupID);
-        }
-        redirect($this->uri->uri_string());
-    }
+		foreach ($coursesArray as $courseID) {
+			$this->model_groupCourse->RemoveCourseFromGroup($courseID, $groupID);
+		}
+	}
 
     //Helpful function for printing to console. Evoke with $this->debug_to_console(value);
-    function debug_to_console( $data ) {
-        if ( is_array( $data ) )
-            $output = "<script>console.log( 'Debug Objects: " . implode( ',', $data) . "' );</script>";
-        else
-            $output = "<script>console.log( 'Debug Objects: " . $data . "' );</script>";
+	function debug_to_console( $data ) {
+		if (is_array( $data ))
+			$output = "<script>console.log( 'Debug Objects: " . implode( ',', $data) . "' );</script>";
+		else
+			$output = "<script>console.log( 'Debug Objects: " . $data . "' );</script>";
 
-        echo $output;
-    }
+		echo $output;
+	}
 }
 ?>
