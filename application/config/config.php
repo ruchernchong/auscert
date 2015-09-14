85353e34d57d55dad870e56043d85494856b1b30
Ru Chern <iruchern@gmail.com>
2015-09-14 22:24:07 +1000

    Fixed pathing

--- application/config/config.php
+++ application/config/config.php
@@ -16,7 +16,7 @@
 | environments.
 |
 */
-$config['base_url'] = 'http://tartiner.uqcloud.net/auscert';
+$config['base_url'] = 'http://tartiner.uqcloud.net/auscert/';
 //$config['base_url'] = 'http://localhost/auscert/';
 
 /*
