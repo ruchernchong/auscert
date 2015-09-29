d151976279465a6f5fde3ce84782c7f12338b0d0
Ru Chern <iruchern@gmail.com>
2015-09-29 21:37:03 +1000

    Added maximum scale for viewport

--- application/views/view_register.php
+++ application/views/view_register.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<meta charset="UTF-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
 	<title>AusCert | Register</title>
 	<!-- <link rel="shortcut icon" href="<?php echo base_url('assets/img/favicon.png'); ?>" /> -->
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/style.css'); ?>" />
