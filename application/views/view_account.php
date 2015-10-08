f18fa0c3ee0d2850708b46e44ff4b9130c036a1e
Ru Chern <iruchern@gmail.com>
2015-10-09 02:42:46 +1000

    Renamed views

---
+++ application/views/view_account.php
@@ -0,0 +1,31 @@
+<div id="page-wrapper">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-lg-12">
+				<h1 class="page-header">My Account 
+					<small>Account Management</small>
+				</h1>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="jumbotron">
+					<h1><center>Page under construction.</center></h1>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<script>
+$("#menu-toggle").click(function(e) {
+	e.preventDefault();
+	$("#wrapper").toggleClass("toggled");
+});
+
+$(document).ready(function() {
+	// $('[data-toggle="tooltip"]').tooltip();
+	$('#pageSettings').removeAttr('href');
+});
+</script>
+</body>
+</html>
