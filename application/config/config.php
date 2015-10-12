0f590a206965b392908c2f8043e6902bfb3e52c4
Ru Chern <iruchern@gmail.com>
2015-10-13 01:24:12 +1000

    Zzz

--- application/config/config.php
+++ application/config/config.php
@@ -16,7 +16,7 @@
 | environments.
 |
 */
-$config['base_url'] = 'http://ruchern.com/auscert/';
+$config['base_url'] = 'http://localhost/auscert/';
 
 /*
 |--------------------------------------------------------------------------
@@ -128,7 +128,8 @@ $config['subclass_prefix'] = 'MY_';
 | DO NOT CHANGE THIS UNLESS YOU FULLY UNDERSTAND THE REPERCUSSIONS!!
 |
 */
-$config['permitted_uri_chars'] = 'a-z 0-9~%.:_\-';
+//$config['permitted_uri_chars'] = 'a-z 0-9~%.:_\-';
+$config['permitted_uri_chars'] = 'a-z 0-9~%.:_()@&\-!';
 
 
 /*
