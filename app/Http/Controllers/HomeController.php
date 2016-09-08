56e4225ddcde3da36371a871e5e5c08e0512ef23
Ru Chern <iruchern@gmail.com>
2016-09-09 06:59:33 +1000

    Preliminary data

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $user = User::all();
+
+        return view('/', compact('user'));
+    }
+}
