6039a233a712c741a220c07ee341837cb3e161d4
Ru Chern <iruchern@gmail.com>
2017-01-15 05:24:23 +0800

    Added Admin role
    Signed-off-by: Ru Chern <iruchern@gmail.com>

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Gate;
+use Illuminate\Contracts\Auth\Access\Gate as GateContract;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -19,12 +19,15 @@ class AuthServiceProvider extends ServiceProvider
     /**
      * Register any authentication / authorization services.
      *
-     * @return void
+     * @param GateContract $gate
      */
-    public function boot()
+    public function boot(GateContract $gate)
     {
-        $this->registerPolicies();
+        $this->registerPolicies($gate);
 
-        //
+        $gate->define('admin', function ($user)
+        {
+            return $user->type == 'admin';
+        });
     }
 }
