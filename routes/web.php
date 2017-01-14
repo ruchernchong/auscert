7912bd9ceb9165dca962f26c11af2bd6fb678ce7
Ru Chern <iruchern@gmail.com>
2017-01-15 05:26:44 +0800

    Added new routes to fit project
    Signed-off-by: Ru Chern <iruchern@gmail.com>

--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,15 @@
 
 Auth::routes();
 
-Route::get('/', 'HomeController@index');
+Route::group(['prefix' => 'dashboard'], function ()
+{
+    Route::get('/', 'HomeController@index');
+    Route::get('/account', 'AccountController@index');
+    Route::get('/help', 'HelpController@index');
+});
+
+Route::group(['prefix' => 'admin'], function ()
+{
+    Route::get('/', 'AdminController@index');
+    Route::resource('course', 'CourseController');
+});
