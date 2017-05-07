f039403c4c9677cdbc160931fb5cbc8dcdc10e2d
Ru Chern Chong <me@ruchern.com>
2017-05-07 11:45:34 +0800

    Add Webpack to Laravel based of Laravel Mix
    Signed-off-by: Ru Chern Chong <me@ruchern.com>

---
+++ webpack.mix.js
@@ -0,0 +1,17 @@
+const {mix} = require('laravel-mix')
+
+mix.browserSync({
+    proxy: 'auscert.dev'
+})
+
+mix.js('resources/assets/js/app.js', 'public/js')
+    .js('resources/assets/js/main.js', 'public/js')
+
+mix.sass('resources/assets/sass/app.scss', 'public/css')
+    .sass('resources/assets/sass/style.scss', 'public/css')
+
+mix.copyDirectory('resources/assets/images', 'public/images')
+
+if (mix.config.inProduction) {
+    mix.version()
+}
