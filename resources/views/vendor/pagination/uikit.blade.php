2e6b3b313eb681ba9e5305c3c7cedb2f8c66817e
Ru Chern Chong <me@ruchern.com>
2017-06-17 16:52:00 +0800

    Add vendor generated views to repo
    Signed-off-by: Ru Chern Chong <me@ruchern.com>

---
+++ resources/views/vendor/pagination/uikit.blade.php
@@ -0,0 +1,31 @@
+@if ($paginator->hasPages())
+    <ul class="uk-pagination" data-uk-margin>
+        @if ($paginator->onFirstPage())
+            <li class="uk-disabled"><span>&laquo;</span></li>
+        @else
+            <li><a href="{{ $paginator->previousPageUrl() }}">&laquo;</a></li>
+        @endif
+
+        @foreach ($elements as $element)
+            @if (is_string($element))
+                <li class="uk-disabled"><span>{{ $element }}</span></li>
+            @endif
+
+            @if (is_array($element))
+                @foreach ($element as $page => $url)
+                    @if ($page === $paginator->currentPage())
+                        <li class="uk-active"><span>{{ $page }}</span></li>
+                    @else
+                        <li><a href="{{ $url }}">{{ $page }}</a></li>
+                    @endif
+                @endforeach
+            @endif
+        @endforeach
+
+        @if ($paginator->hasMorePages())
+            <li><a href="{{ $paginator->nextPageUrl() }}" rel="next">&raquo;</a></li>
+        @else
+            <li class="uk-disabled"><span>&raquo;</span></li>
+        @endif
+    </ul>
+@endif
